0146aab6b4db991593599cade29d57f0e3d43601
Apedy tech <115509032+Apedy@users.noreply.github.com>
2023-03-14 23:39:10 +0900

    Update display.js

---
+++ behavior_pack/scripts/server/config.js
@@ -0,0 +1,12 @@
+/**
+ * これはMinePaintのコンフィグファイルです。
+ * ここの項目を書き換えることで仕様を変更することが出来ます。
+ *
+ * This is MinePaint's config file.
+ * You can change the specifications by rewriting the elements here.
+ */
+
+export const config = {
+	buildNumber: 4725,
+	opTag: "admin"
+};
