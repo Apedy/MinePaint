5430fe97122c74e4f79d59c4a75dbc8059aa73db
Apedy tech <115509032+Apedy@users.noreply.github.com>
2023-03-14 23:38:07 +0900

    Update script.js

--- behavior_pack/scripts/server/script.js
+++ behavior_pack/scripts/server/script.js
@@ -1,41 +1,37 @@
+/*!
+ * Copyright (C) 2023 Apedy
+ * This file is part of MinePaint.
+ *
+ * MinePaint is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MinePaint is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this MinePaint. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as mc from '@minecraft/server';
+import { World } from '../lib/minecraft';
+
+import * as admin from '../modules/admin/index';
+import { MainPanel, SystemPanel } from '../modules/panel/index';
 import { lang } from '../@minepaint/lang';
-import * as ui from './display';
+import { config } from './config';
 import './support';
-import {
-	config,
-	dimension,
 
-	//? Data
-	worldData,
-	playerData,
-	reportData,
-	oreData,
-
-	//? Option
-	kitOptions,
-	mapOptions,
-	reportOptions,
-
-	//? Class
-	RDStack,
-	ODStack,
-	PDMhs,
-	RDMhs,
-	ODMhs,
-
-	//? Function
-	runGame,
-	resetGame,
-	loginProcess,
-
-	//? Support
-	runCommands,
-	getPlayerList
-} from '../@minepaint/admin';
+const mcLib = new World("overworld");
 
 
 mc.system.events.beforeWatchdogTerminate.subscribe(eventData => eventData.cancel = true);
+mc.world.events.beforeItemUseOn.subscribe(eventData => {
+	if (mcLib.getPlayerList({ excludeGameModes: [mc.GameMode.creative] }).map(player => player.id).includes(eventData.source.id)) eventData.cancel = true;
+});
 
 mc.world.getDimension("overworld").runCommandAsync(`say Build Number: §e${config.buildNumber}`);
 
@@ -43,36 +39,31 @@ mc.world.events.beforeItemUse.subscribe(eventData => {
 	const {	source } = eventData;
 
 	if (eventData.item.typeId === "minecraft:book") {
-		if (worldData.system.startUp) {
-			if (worldData.system.game) source.hasTag(config.opTag) ? ui.System.gameResetPanel() : null;
-			else source.isSneaking && source.hasTag(config.opTag) ? ui.System.systemPanel(source) : ui.Main.mainPanel(source);
+		if (admin.worldData.system.startUp) {
+			if (admin.worldData.system.game && source.hasTag(config.opTag)) SystemPanel.resetGame(source);
+			else source.isSneaking && source.hasTag(config.opTag) ? SystemPanel.show(source) : MainPanel.show(source);
 		}
 		else if (source.hasTag(config.opTag)) {
 			try {
-				ui.System.startupPanel(source);
+				SystemPanel.startup(source);
 			}
-			catch (e) {
-				worldData.system.startUp = false;
-				runCommands(source, `say §l§e(§cERROR§e) §fcode:#FFFF`);
-				ui.errorPanel(source, `${lang.system.message.startUp.again}\n${e}`);
+			catch {
+				admin.worldData.system.startUp = false;
+				mcLib.runCommands(source, `say §l§e(§cERROR§e) §fcode:#FFFF`);
+				panel.Error.show(source, `${lang.system.message.startUp.again}` );
 			}
 		}
-		else return ui.errorPanel(source, lang.system.message.startUp.not);
-	}
-	else if (eventData.item.typeId === "minecraft:apple") {
-		runCommands(source, "say test");
-		ui.errorPanel(source, "test");
+		else return panel.Error.show(source, lang.system.message.startUp.not);
 	}
 	else return;
 });
-
 
 //* ore event
 mc.world.events.blockBreak.subscribe(eventData => {
 	const { player } = eventData;
 	const block = {
-		id: eventData.brokenBlockPermutation.type.id,
-		location: `${eventData.block.location.x} ${eventData.block.location.y} ${eventData.block.location.z}`,
+		typeId: eventData.brokenBlockPermutation.type.id,
+		location: eventData.block.location,
 		permutation: eventData.brokenBlockPermutation,
 		set: () => eventData.block.setPermutation(block.permutation)
 	};
@@ -85,20 +76,20 @@ mc.world.events.blockBreak.subscribe(eventData => {
 		"minecraft:coal_ore": { item: 1, time: 20 }
 	};
 
-	if (worldData.system.game && Object.keys(blockData).includes(block.id)) {
-		oreData.manager(new ODMhs().add, block.location, new ODStack(block.id, blockData[block.id].time));
+	if (admin.worldData.system.game && Object.keys(blockData).includes(block.typeId)) {
+		admin.OreState.create(block.location, block.typeId, blockData[block.typeId].time);
 
-		if (/^minecraft:deepslate/i.test(block.id)) runCommands(dimension, `setblock ${block.location} cobbled_deepslate`);
-		else runCommands(dimension, `setblock ${block.location} cobblestone`);
+		if (/^minecraft:deepslate/i.test(block.id)) mcLib.runCommands(mcLib.dimension, `setblock ${block.location.x} ${block.location.y} ${block.location.z} cobbled_deepslate`);
+		else mcLib.runCommands(mcLib.dimension, `setblock ${block.location.x} ${block.location.y} ${block.location.z} cobblestone`);
 
-		switch (playerData.find(playerData.queryOptions.inTeam, player)) {
-			// case "red": runCommands(player, `give @s concrete_powder ${blockData[block.id].item} 6`); break;
-			// case "blue": runCommands(player, `give @s concrete_powder ${blockData[block.id].item} 3`); break;
-			case "blue": runCommands(player, `give @s stone ${blockData[block.id].item}`); break;
-			case "red": runCommands(player, `give @s cobblestone ${blockData[block.id].item}`); break;
+		switch (admin.PlayerStatus.find(player, admin.PlayerStatus.queryOptions.inTeam)) {
+			// case "red": mcLib.runCommands(player, `give @s concrete_powder ${blockData[block.id].item} 6`); break;
+			// case "blue": mcLib.runCommands(player, `give @s concrete_powder ${blockData[block.id].item} 3`); break;
+			case "blue": mcLib.runCommands(player, `give @s stone ${blockData[block.typeId].item}`); break;
+			case "red": mcLib.runCommands(player, `give @s cobblestone ${blockData[block.typeId].item}`); break;
 		}
 	}
-	else if (worldData.system.game && ["minecraft:cobblestone", "minecraft:cobbled_deepslate"].includes(block.id)) return block.set();
+	else if (admin.worldData.system.game && ["minecraft:cobblestone", "minecraft:cobbled_deepslate"].includes(block.typeId)) return block.set();
 });
 
 
@@ -106,20 +97,21 @@ mc.world.events.blockBreak.subscribe(eventData => {
 mc.system.run(function tick() {
 	mc.system.run(tick);
 
-	runCommands(dimension,
-		`effect @a night_vision 11 0 true`,
-		`effect @a saturation 1 255 true`,
-		`tp @a[y=-65,dy=-100] 0 -60 0 0 0`
+	mcLib.runCommands(mcLib.dimension,
+		`effect @a saturation 1 255 true`
 	);
 
-	if (worldData.system.game) {
+	if (admin.worldData.system.game) {
 		//? game event
-		worldData.game.timer--;
+		admin.worldData.game.timer--;
 
-		runCommands(dimension,
-			`title @a actionbar "${Math.floor(worldData.game.timer / 20 * Math.pow(10, 1) ) / Math.pow(10, 1)}"`,
-			`function game/system/${worldData.game.getMap()}`
-		);
+		if (admin.worldData.game.timer > 0) {
+			mcLib.runCommands(mcLib.dimension,
+				`effect @a night_vision 11 0 true`,
+				`title @a actionbar "${Math.floor(admin.worldData.game.timer / 20 * Math.pow(10, 1) ) / Math.pow(10, 1)}"`,
+				`function game/system/${admin.worldData.game.getMap()}`
+			);
+		}
 
 		/**
 		 * ^: (_ [to] _s), meaning [to]. / _ is the seconds.
@@ -127,43 +119,54 @@ mc.system.run(function tick() {
 		 * ..: Seconds to end.
 		 */
 		//? +30^+6s^
-		if (worldData.game.timer - 120 >= worldData.setting.timer * 20 && worldData.game.timer % 20 === 0) runCommands(dimension, `playsound note.hat @a`);
+		if (admin.worldData.game.timer - 120 >= admin.worldData.setting.timer * 20 && admin.worldData.game.timer % 20 === 0) mcLib.runCommands(mcLib.dimension, `playsound note.hat @a`);
 		//? +5^+1s
-		else if (worldData.game.timer > worldData.setting.timer * 20 && worldData.game.timer % 20 === 0) runCommands(dimension, `title @a title §e${worldData.game.timer / 20 - worldData.setting.timer}`, `playsound note.harp @a`);
+		else if (admin.worldData.game.timer > admin.worldData.setting.timer * 20 && admin.worldData.game.timer % 20 === 0) mcLib.runCommands(mcLib.dimension, `title @a title §e${admin.worldData.game.timer / 20 - admin.worldData.setting.timer}`, `playsound note.harp @a`);
 		//? +0s
-		else if (worldData.game.timer === worldData.setting.timer * 20 && worldData.game.timer % 20 === 0) runCommands(dimension, `function game/process/start_${worldData.game.getMap()}`);
+		else if (admin.worldData.game.timer === admin.worldData.setting.timer * 20 && admin.worldData.game.timer % 20 === 0) mcLib.runCommands(mcLib.dimension, `function game/process/start_${admin.worldData.game.getMap()}`);
 
 		//? ..30s
-		if (worldData.game.timer === 600) runCommands(dimension, `tellraw @a {"rawtext": [{"translate": "${lang.system.message.game.time[30]}"}]}`, `playsound random.toast @a`);
+		if (admin.worldData.game.timer === 600) mcLib.runCommands(mcLib.dimension, `tellraw @a {"rawtext": [{"translate": "${lang.system.message.game.time[30]}"}]}`, `playsound random.toast @a`);
 		//? ..10s
-		if (worldData.game.timer === 200) runCommands(dimension, `tellraw @a {"rawtext": [{"translate": "${lang.system.message.game.time[10]}"}]}`, `playsound random.toast @a`);
+		if (admin.worldData.game.timer === 200) mcLib.runCommands(mcLib.dimension, `tellraw @a {"rawtext": [{"translate": "${lang.system.message.game.time[10]}"}]}`, `playsound random.toast @a`);
 		//? ..10^5s
-		if (worldData.game.timer < 200 && worldData.game.timer > 100 && worldData.game.timer % 10 === 0) runCommands(dimension, `playsound note.hat @a`);
+		if (admin.worldData.game.timer <= 200 && admin.worldData.game.timer >= 100 && admin.worldData.game.timer % 10 === 0) mcLib.runCommands(mcLib.dimension, `playsound note.hat @a`);
 		//? ..5^0s
-		if (worldData.game.timer <= 100 && worldData.game.timer > 0 && worldData.game.timer % 5 === 0) runCommands(dimension, `playsound note.hat @a`);
+		if (admin.worldData.game.timer === 100 && admin.worldData.game.timer >= 0 && admin.worldData.game.timer % 5 === 0) mcLib.runCommands(mcLib.dimension, `playsound note.hat @a`);
 		//? ..0s
-		if (worldData.game.timer === 0) {
-			resetGame();
-		}
+		if (admin.worldData.game.timer === 0) admin.quitGame();
+		if (admin.worldData.game.timer === -200) admin.resultGame();
 
 		//? ore event
-		if (oreData.find()) oreData.find().map(location => {
-			oreData.manager(new ODMhs().update, location);
-
-			if (oreData.find(location).time <= 0) {
-				runCommands(dimension, `setblock ${location} ${oreData.find(location).ore}`);
-				oreData.manager(new ODMhs().delete, location);
+		if (admin.OreState.get().length) admin.OreState.get().forEach(content => {
+			if (content.respawnTime <= 0) {
+				mcLib.runCommands(mcLib.dimension, `setblock ${content.location} ${admin.OreState.find(content.location).oreType}`);
+				admin.OreState.delete(content.location);
 			}
-			else return;
+			else admin.OreState.countDown(content.location);
 		});
 		else return;
 	}
 	else return;
 });
 
-mc.world.events.playerJoin.subscribe(eventData => {
-	const {player} = eventData;
+mc.world.events.playerSpawn.subscribe(eventData => {
+	const { player } = eventData;
 
-	if (worldData.system.startUp) loginProcess(player);
+	if (admin.worldData.system.startUp) admin.loginProcess(player);
 	else return;
+});
+
+mc.system.events.scriptEventReceive.subscribe(eventData => {
+	const { id, sourceEntity } = eventData;
+	const msg = eventData.message;
+
+	if (id === "minepaint:team") {
+		if (msg === "blue") admin.PlayerStatus.set(admin.PlayerStatus.queryOptions.inTeam, sourceEntity, "blue");
+		if (msg === "red") admin.PlayerStatus.set(admin.PlayerStatus.queryOptions.inTeam, sourceEntity, "red");
+	}
+	if (id === "minepaint:open") {
+		MainPanel.show(sourceEntity);
+		mcLib.runCommands(sourceEntity, "give @s apple");
+	}
 });
